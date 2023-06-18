fff5cd0c0e13ea6b02ca31f464ca6eb5cc04f27b
torondil <bulavskipavel@gmail.com>
2023-06-18 17:21:44 +0200

    feat: add products

---
+++ src/component/ProductItem/ProductItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Button } from '../Button/Button'
+
+export const ProductItem = ({product, className, onAdd}) => {
+    const onAddHandler = () => {
+        onAdd(product)
+    }
+    
+  return (
+    <div className={'product ' + className}>
+        <div className={'img'}></div>
+        <div className={'title'}>{product.title}</div>
+        <div className={'description'}>{product.description}</div>
+        <div className={'price'}>
+            <span>Стоимость: <b>{product.price}</b></span>
+        </div>
+        <Button className={'add-btn'} onClick={onAddHandler}>
+            Добавить в корзину
+        </Button>
+    </div>
+  )
+}
